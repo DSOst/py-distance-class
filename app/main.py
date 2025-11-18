be5d7871338bdee20cdc3a9f5c985291806ee50f
Dmytro Ostapenko <dr.dm.ostapenko@gmail.com>
2025-11-18 17:20:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -29,8 +29,6 @@ class Distance:
         return self
 
     def __mul__(self, other: Union["Distance", int, float]) -> "Distance":
-        if isinstance(other, Distance):
-            new_km = self.km * other.km
         if isinstance(other, (int, float)):
             new_km = self.km * other
         else:
